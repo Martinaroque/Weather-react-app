768c7f53f49fecdd055acf2cd206afef9459c136
Martinaroque <martirocklx@hotmail.com>
2023-07-15 22:56:53 +0100

    Adding react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+export default function Weather() {
+  let [city, setCity] = useState("");
 
-function App() {
+  function showTemperature(response) {
+    let temp = document.querySelector("#temp");
+    temp.innerHTML = Math.round(response.data.main.temp);
+    let descrip = document.querySelector("#descrip");
+    descrip.innerHTML = response.data.weather[0].description;
+    let hum = document.querySelector("#hum");
+    hum.innerHTML = response.data.main.humidity;
+    let wind = document.querySelector("#wind");
+    wind.innerHTML = response.data.wind.speed;
+    let icon = document.querySelector("#icon");
+    icon.setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+    getForecast(response.data.coord);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9cb72bec958f8fb02391985ed7b219d2&units=metric`;
+    axios.get(url).then(showTemperature);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="type a city" onChange={updateCity} />
+      <input type="submit" className="btn btn-primary m-2 p-2" />
+    </form>
   );
-}
+  function formatDay(timeStamp) {
+    let date = new Date(timeStamp * 1000);
+    let day = date.getDay();
+    let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+    return days[day];
+  }
+  function showForecast(response) {
+    let forecasting = response.data.daily;
 
-export default App;
+    let forecastElement = document.querySelector("#forecast");
+
+    let forecastHtml = ` <div class="row">`;
+
+    forecasting.forEach(function (forecastDay, index) {
+      if (index < 6) {
+        forecastHtml =
+          forecastHtml +
+          ` <div class="col-2">
+        <div class="forescast-date">
+        ${formatDay(forecastDay.dt)}
+    </div>
+    <div>
+        <img src="https://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png" alt="" width="42px"/>
+    </div>
+    <div class="forecast-temp">
+        <span class="forescast-temp-max" >${Math.round(
+          forecastDay.temp.max
+        )}º</span> 
+        <span class="forescast-temp-min">${Math.round(
+          forecastDay.temp.min
+        )}º</span> 
+    </div>
+    </div>`;
+      }
+    });
+
+    forecastHtml = forecastHtml + `</div>`;
+    forecastElement.innerHTML = forecastHtml;
+  }
+  function getForecast(coordinates) {
+    let apiKey = "9cb72bec958f8fb02391985ed7b219d2";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(showForecast);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="container">
+      <h1>Weather App</h1>
+      <br />
+      <App />
+      <br />
+      <ul className="descriptionWeather">
+        <li>
+          Temperature:<span id="temp"></span>
+        </li>
+        <li>
+          Description:<span id="descrip"></span>
+        </li>
+        <li>
+          Humidity:<span id="hum"></span>
+        </li>
+        <li>
+          Wind:<span id="wind"></span> Km/h
+        </li>
+        <li>
+          <img src="" alt="" id="icon" />
+        </li>
+      </ul>
+      <div class="weather-forecast" id="forecast">
+        <div class="row">
+          <div class="col-2">
+            <div class="forescast-date"></div>
+            <div>
+              <img src="#" alt="" width="42px" />
+            </div>
+            <div class="forecast-temp">
+              <span class="forescast-temp-max"></span>
+              <span class="forescast-temp-min"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </React.StrictMode>
 );
 
